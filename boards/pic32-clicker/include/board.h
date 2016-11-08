1b5bd30eaafea3e517f7a3320914976572d30773
Neil Jones <neil.jones@imgtec.com>
2016-11-08 16:55:27 +0000

    boards: pic32-clicker: Add support for the MikroE Clicker board.
    This board features a pic32mx470f512h PIC32 device with a MIPS core.

---
+++ boards/pic32-clicker/include/board.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016, Imagination Technologies Limited and/or its
+ *                 affiliated group companies.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    boards_pic32-clicker MikroE PIC32 Clicker
+ * @ingroup     boards
+ * @brief       board configuration for the MikroE PIC32 Clicker
+ * @details
+ * see:
+ * http://www.mikroe.com/pic32/pic32mx-clicker/
+ * For more information on the board.
+ *
+ * @{
+ *
+ * @file
+ * @brief       board configuration for the MikroE PIC32 Clicker
+ *
+ * @author      Neil Jones <Neil.Jones@imgtec.com>
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "p32mx470f512h.h"
+
+/**
+ * @brief Set how many increments of the count register per uS
+ *        needed by the timer code.
+ */
+#define TICKS_PER_US (48)
+
+/**
+ * @brief We are using an External Interrupt Controller (all pic32 devices use this mode)
+ */
+#define EIC_IRQ (1)
+
+/**
+ * @brief   Board level initialisation
+ */
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _BOARD_H_ */
+/** @} */
