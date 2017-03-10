dbafb973b140b75a30e78c9c7a9b49f6a92afaac
Joseph Noir <joseph.noir@gmail.com>
2017-03-10 11:18:45 +0100

    cpp11-compat: address cppcheck warnings

--- sys/cpp11-compat/include/riot/thread.hpp
+++ sys/cpp11-compat/include/riot/thread.hpp
@@ -27,6 +27,7 @@
 #include "time.h"
 #include "thread.h"
 
+#include <array>
 #include <tuple>
 #include <atomic>
 #include <memory>
@@ -345,7 +346,7 @@ thread::thread(F&& f, Args&&... args)
   unique_ptr<func_and_args> p(
     new func_and_args(m_data.get(), forward<F>(f), forward<Args>(args)...));
   m_handle = thread_create(
-    m_data->stack, stack_size, THREAD_PRIORITY_MAIN - 1, 0,
+    m_data->stack.data(), stack_size, THREAD_PRIORITY_MAIN - 1, 0,
     &thread_proxy<func_and_args>, p.get(), "riot_cpp_thread");
   if (m_handle >= 0) {
     p.release();
