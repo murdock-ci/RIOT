f9b9bf5c78a3132cf3cac7ac9589e4ad0f9594ae
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-04-26 14:27:17 +0200

    s cpu

--- cpu/stm32f7/include/periph_cpu.h
+++ cpu/stm32f7/include/periph_cpu.h
@@ -44,39 +44,6 @@ enum {
     PORT_K = 10             /**< port K */
 };
 
-#ifndef DOXYGEN
-/**
- * @brief   Override ADC resolution values
- * @{
- */
-#define HAVE_ADC_RES_T
-typedef enum {
-    ADC_RES_6BIT  = (0x3 << ADC_CR1_RES_Pos),   /**< ADC resolution: 6 bit */
-    ADC_RES_8BIT  = (0x2 << ADC_CR1_RES_Pos),   /**< ADC resolution: 8 bit */
-    ADC_RES_10BIT = (0x1 << ADC_CR1_RES_Pos),   /**< ADC resolution: 10 bit */
-    ADC_RES_12BIT = (0x0 << ADC_CR1_RES_Pos),   /**< ADC resolution: 12 bit */
-    ADC_RES_14BIT = (0xfe),                     /**< not applicable */
-    ADC_RES_16BIT = (0xff)                      /**< not applicable */
-} adc_res_t;
-/** @} */
-#endif /* ndef DOXYGEN */
-
-/**
- * @brief   ADC line configuration values
- */
-typedef struct {
-    gpio_t pin;             /**< pin to use */
-    uint8_t chan;           /**< internal channel the pin is connected to */
-} adc_conf_t;
-
-/**
- * @brief   DAC line configuration data
- */
-typedef struct {
-    gpio_t pin;             /**< pin connected to the line */
-    uint8_t chan;           /**< DAC device used for this line */
-} dac_conf_t;
-
 #ifdef __cplusplus
 }
 #endif
