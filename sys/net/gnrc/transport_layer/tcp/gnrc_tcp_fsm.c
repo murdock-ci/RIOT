88682044ff44fc881c60aabbc26200d887421d00
Simon Brummer <simon.brummer@posteo.de>
2017-05-09 09:29:33 +0200

    Reduced scope of local variables in fsm

--- sys/net/gnrc/transport_layer/tcp/gnrc_tcp_fsm.c
+++ sys/net/gnrc/transport_layer/tcp/gnrc_tcp_fsm.c
@@ -215,9 +215,7 @@ static int _transition_to(gnrc_tcp_tcb_t *tcb, fsm_state_t state)
  */
 static int _fsm_call_open(gnrc_tcp_tcb_t *tcb)
 {
-    gnrc_pktsnip_t *out_pkt = NULL;     /* Outgoing packet */
-    uint16_t seq_con = 0;               /* Sequence number consumption of outgoing packet */
-    int ret = 0;                        /* Return value */
+    int ret = 0;
 
     DEBUG("gnrc_tcp_fsm.c : _fsm_call_open()\n");
     tcb->rcv_wnd = GNRC_TCP_DEFAULT_WINDOW;
@@ -243,6 +241,8 @@ static int _fsm_call_open(gnrc_tcp_tcb_t *tcb)
         }
 
         /* Send SYN */
+        gnrc_pktsnip_t *out_pkt = NULL;
+        uint16_t seq_con = 0;
         _pkt_build(tcb, &out_pkt, &seq_con, MSK_SYN, tcb->iss, 0, NULL, 0);
         _pkt_setup_retransmit(tcb, out_pkt, false);
         _pkt_send(tcb, out_pkt, seq_con, false);
@@ -261,10 +261,8 @@ static int _fsm_call_open(gnrc_tcp_tcb_t *tcb)
  */
 static int _fsm_call_send(gnrc_tcp_tcb_t *tcb, void *buf, size_t len)
 {
-    gnrc_pktsnip_t *out_pkt = NULL;     /* Outgoing packet */
-    uint16_t seq_con = 0;               /* Sequence number consumption (out_pkt) */
-
     DEBUG("gnrc_tcp_fsm.c : _fsm_call_send()\n");
+
     size_t payload = (tcb->snd_una + tcb->snd_wnd) - tcb->snd_nxt;
 
     /* Check if window is open and all packets were transmitted */
@@ -275,6 +273,8 @@ static int _fsm_call_send(gnrc_tcp_tcb_t *tcb, void *buf, size_t len)
         payload = (payload < len) ? payload : len;
 
         /* Calculate payload size for this segment */
+        gnrc_pktsnip_t *out_pkt = NULL;
+        uint16_t seq_con = 0;
         _pkt_build(tcb, &out_pkt, &seq_con, MSK_ACK, tcb->snd_nxt, tcb->rcv_nxt, buf, payload);
         _pkt_setup_retransmit(tcb, out_pkt, false);
         _pkt_send(tcb, out_pkt, seq_con, false);
@@ -294,10 +294,8 @@ static int _fsm_call_send(gnrc_tcp_tcb_t *tcb, void *buf, size_t len)
  */
 static int _fsm_call_recv(gnrc_tcp_tcb_t *tcb, void *buf, size_t len)
 {
-    gnrc_pktsnip_t *out_pkt = NULL;     /* Outgoing packet */
-    uint16_t seq_con = 0;               /* Sequence number consumption of outgoing packet */
-
     DEBUG("gnrc_tcp_fsm.c : _fsm_call_recv()\n");
+
     if (ringbuffer_empty(&tcb->rcv_buf)) {
         return 0;
     }
@@ -310,6 +308,8 @@ static int _fsm_call_recv(gnrc_tcp_tcb_t *tcb, void *buf, size_t len)
         tcb->rcv_wnd = ringbuffer_get_free(&(tcb->rcv_buf));
 
         /* Send ACK to anounce window update */
+        gnrc_pktsnip_t *out_pkt = NULL;
+        uint16_t seq_con = 0;
         _pkt_build(tcb, &out_pkt, &seq_con, MSK_ACK, tcb->snd_nxt, tcb->rcv_nxt, NULL, 0);
         _pkt_send(tcb, out_pkt, seq_con, false);
     }
@@ -325,13 +325,14 @@ static int _fsm_call_recv(gnrc_tcp_tcb_t *tcb, void *buf, size_t len)
  */
 static int _fsm_call_close(gnrc_tcp_tcb_t *tcb)
 {
-    gnrc_pktsnip_t *out_pkt = NULL;     /* Outgoing packet */
-    uint16_t seq_con = 0;               /* Sequence number consumption of outgoing packet */
-
     DEBUG("gnrc_tcp_fsm.c : _fsm_call_close()\n");
+
     if (tcb->state == FSM_STATE_SYN_RCVD || tcb->state == FSM_STATE_ESTABLISHED ||
         tcb->state == FSM_STATE_CLOSE_WAIT) {
+
         /* Send FIN packet */
+        gnrc_pktsnip_t *out_pkt = NULL;
+        uint16_t seq_con = 0;
         _pkt_build(tcb, &out_pkt, &seq_con, MSK_FIN_ACK, tcb->snd_nxt, tcb->rcv_nxt, NULL, 0);
         _pkt_setup_retransmit(tcb, out_pkt, false);
         _pkt_send(tcb, out_pkt, seq_con, false);
@@ -358,9 +359,6 @@ static int _fsm_call_close(gnrc_tcp_tcb_t *tcb)
  */
 static int _fsm_call_abort(gnrc_tcp_tcb_t *tcb)
 {
-    gnrc_pktsnip_t *out_pkt = NULL;     /* Outgoing packet */
-    uint16_t seq_con = 0;               /* Sequence number consumption of outgoing packet */
-
     DEBUG("gnrc_tcp_fsm.c : _fsm_call_abort()\n");
 
     /* A reset must be sent in case the TCB state is in one of those cases */
@@ -369,6 +367,8 @@ static int _fsm_call_abort(gnrc_tcp_tcb_t *tcb)
         tcb->state == FSM_STATE_CLOSE_WAIT) {
 
         /* Send RST packet without retransmit */
+        gnrc_pktsnip_t *out_pkt = NULL;
+        uint16_t seq_con = 0;
         _pkt_build(tcb, &out_pkt, &seq_con, MSK_RST, tcb->snd_nxt, tcb->rcv_nxt, NULL, 0);
         _pkt_send(tcb, out_pkt, seq_con, false);
     }
